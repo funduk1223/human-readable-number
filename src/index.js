caab5210baa0e721852cc97a7047f8ae770e28c9
Mikhail Akhmedzyanov <mikhail.akhmedzyanov@gmail.com>
2020-10-05 10:08:01 +0300

    init: done human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function toReadable (number) {
-  
+  let numbArray = (String(number)).split('').map(function(a) {
+    return Number(a);
+  });
+
+  function seporator(numbArray){
+    if (numbArray[0] === 1) {
+      result += tens[numbArray[1]]
+    }
+    else if (numbArray[0] !== 0){
+      numbArray[1] === 0 ? result+=hundreds[numbArray[0]-2] : result=hundreds[numbArray[0]-2] + ' ' + units[numbArray[1]]; 
+    }
+
+    else if (numbArray[0] === 0){
+      if (numbArray[1] === 0) {
+        result = '';
+      }
+      else result += units[numbArray[1]];
+    }
+
+    return result;
+  }
+
+  var units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  var tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  var hundreds = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  var result = '';
+
+  if (numbArray.length === 1){
+    result += units[numbArray];
+  }
+
+  else if (numbArray.length === 2){
+    result = seporator(numbArray);
+  }
+
+  else if (numbArray.length === 3){
+    
+    result = units[numbArray[0]] + ' hundred ' + seporator(numbArray.slice(-2))
+  }
+  return result.replace(/\s$/,'');
 }
+
+
+
